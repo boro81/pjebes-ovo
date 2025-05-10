c3e406b72f382070c64ccd2331df0875c50ae6f7
boro81 <grabborislav@gmail.com>
2025-05-11 00:15:43 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hodnik",
+      "name": "hodnik",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.10524969051359,
+        "pitch": 1.0848309863257501,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7556760447621702,
+          "pitch": 0.23003352984032155,
+          "rotation": 0,
+          "target": "1-wc"
+        },
+        {
+          "yaw": 3.1029069787433166,
+          "pitch": 0.39895100049430887,
+          "rotation": 0,
+          "target": "3-djecija-saoba"
+        },
+        {
+          "yaw": 2.30317558270548,
+          "pitch": 0.1480038463364277,
+          "rotation": 0,
+          "target": "2-soba"
+        },
+        {
+          "yaw": -1.8699698068681005,
+          "pitch": 0.29104441723973906,
+          "rotation": 0,
+          "target": "4-dnevna"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.438483730796288,
+          "pitch": 0.643907131903898,
+          "title": "Dyson",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-wc",
+      "name": "WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-soba",
+      "name": "soba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.32922235963189905,
+          "pitch": 0.2516872047040746,
+          "rotation": 0,
+          "target": "0-hodnik"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-djecija-saoba",
+      "name": "djecija saoba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.39142626337136655,
+          "pitch": 0.2647734857134836,
+          "rotation": 0,
+          "target": "0-hodnik"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dnevna",
+      "name": "dnevna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5763207981404328,
+          "pitch": 0.23472020878768696,
+          "rotation": 0,
+          "target": "0-hodnik"
+        },
+        {
+          "yaw": 2.378179164801881,
+          "pitch": 0.25543659769736493,
+          "rotation": 0,
+          "target": "5-balkon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-balkon",
+      "name": "balkon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.225811249600131,
+          "pitch": 0.6596053119778009,
+          "rotation": 0,
+          "target": "4-dnevna"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
